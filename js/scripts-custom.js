df9523fd155bc2fe5e040d04116dcf5c6bd831eb
vadi4 <vadich92@gmail.com>
2024-04-12 16:40:38 +0300

    added countdown submit

--- js/scripts-custom.js
+++ js/scripts-custom.js
@@ -21,7 +21,39 @@ if( document.querySelector('.pin-code-wrap') ) {
 		});
 
 	});
+};
 
+const stringCoundown = 'Повторно запросить код через <span data-countdown-submit="true"></span>';
+let remainingTime = 60;
 
+function countDownSubmit() {
+
+	const $countDownPlace = document.querySelector('[data-place-countdown]');
+
+	if( !$countDownPlace ) return;
+
+	$countDownPlace.innerHTML = stringCoundown;
+
+	const $countDownTimerPlace = document.querySelector('[data-countdown-submit]');
+	const $submitBtn = document.querySelector('[data-resend-code]');
+
+	$submitBtn.classList.add('disabled-submit');
+
+	const timer = setInterval(() => {
+		const minutes = Math.floor(remainingTime / 60);
+		const seconds = remainingTime % 60;
+
+		$countDownTimerPlace.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+		remainingTime--;
+
+		if (remainingTime < 0) {
+			clearInterval(timer);
+			$countDownTimerPlace.innerText = '00:00';
+
+			$submitBtn.classList.remove('disabled-submit');
+			$countDownPlace.innerHTML = '';
+		}
+	}, 1000);
 
 }
