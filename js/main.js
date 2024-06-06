9e8a287eb4e73986717e1db4c62176f86260e4d3
vadi4 <vadich92@gmail.com>
2024-06-06 12:09:42 +0300

    upd verst rewards

--- js/main.js
+++ js/main.js
@@ -11,6 +11,13 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
 //////////////////////////////////////////////////////////////////
+
+// [ Enable tooltips ]
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
 // [ Top Menu Shadow ]
 
 window.onscroll = function () {
